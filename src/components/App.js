a0968b6d6be8c774ef69189bb9ecf92ef0e2dc12
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2021-06-10 21:44:42 -0400

    Added REDUX, Router - Initial Setups
    Added REDUX, Router - Initial Setups

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//Dummy Compents to do the active routing
+
+const Header = () => {
+    return <h2>Header</h2>;
+};
+
+const Dashboard = () => {
+    return <h2>Dashboard</h2>;
+};
+
+const SurveyNew = () => {
+    return <h2>SurveyNew</h2>;
+};
+
+const Landing = () => {
+    return <h2>Landing</h2>;
+};
 
 const App = () => {
     return (
-        <div>
-            <h1>Emaily - Feedback - APP</h1>
+        <div> {/* Left it for the CSS setup later */}
+        <Header />
+            <BrowserRouter>
+                <Route path='/' component={Landing} exact />
+                <Route path='/surveys' component={Dashboard} exact />
+                <Route path='/surveys/new' component={SurveyNew} exact />
+            </BrowserRouter>
         </div>
     );
 }
